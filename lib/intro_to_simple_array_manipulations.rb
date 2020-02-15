51d1ca30fdde14db6ebdcef1a35943ab4defd791
Ali G Junejo <aligohar_junejo@live.com>
2020-02-15 21:41:41 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,62 @@
-def using_push(Array, String)
-artists = ["Linkin Park", "Backstreet Boys", "John Legend"]
-using_push.push
+
+def using_push(array, violet)
+    array.push("violet")
+end
+
+
+def using_unshift(array, violet)
+    array.unshift("Staten Island")
+end
+
+
+def using_pop(array)
+    array.pop
+end
+
+
+def pop_with_args(array)
+    array.pop(2)
+end
+
+
+def using_shift(array)
+    array.shift 
+end
+
+
+def shift_with_args(array)
+    array.shift(2)
+end
+
+
+def using_concat(array1, array2)
+    array1.concat(array2)
+end
+
+
+def using_insert(array, element)
+    array.insert(4, "Python")
+end
+
+
+def using_uniq(array)
+    array.uniq
+end
+
+
+def using_flatten(array)
+    array.flatten
+end
+
+
+def using_delete(array, string)
+    array.delete("Steven")
+end
+
+
+def using_delete_at(array, number)
+    array.delete_at(2)
+end
+
+
+
